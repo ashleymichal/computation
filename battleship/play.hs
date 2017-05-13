f00a3daa7018a21a9499b33cdef64a7871beac70
Sukant Hajra <rrudbskr5g@snkmail.com>
2017-05-12 21:52:24 -0500

    More Battleship
    I was playing around with thinking about the problem in terms of types.
    This is a really popular way to think about a problem when functional
    programming in a language with a good type system and checker.
    
    The next step is to define operations with type signatures.
    
    The last thing to do is implement them.

--- battleship/play.hs
+++ battleship/play.hs
@@ -3,9 +3,29 @@ import           Data.HashMap.Strict as HM
 
 newtype Latitude = Latitude Int
 newtype Longitude = Longitude Int
-data Coord = Coord { _latitude :: Latitude, _longitude :: Longitude }
+newtype XOffset = XOffset Int
+newtype YOffset = YOffset Int
 
-newtype Ship = Ship (HM.HashMap Coord ShipStatus)
+data Coord = Coord Latitude Longitude
+data Offset = Offset XOffset YOffset
+
+data ShipTemplate = ShipTemplate [Offset]
+data Rotation = Rot0 | Rot90 | Rot180 | Rot270
+data Ship = Ship (HM.HashMap Coord ShipStatus)
 data ShipStatus = Damaged | Floating
 
 data Response = Hit | Miss | HitAndSunk
+data Pin = RedPin | WhitePin
+data Board = Board (HM.HashMap Coord Pin)
+newtype Reality = Reality [Ship]
+
+data Player = Player Board Reality
+newtype Attacker = Attacker Player
+newtype Attacked = Attacked Player
+newtype Attack = Attack Coord
+data Turn = Turn Attacker Attacked Attack
+data TurnResult = TurnResult Attacker Attacked
+
+
+attack :: Turn -> TurnResult
+attack = undefined
